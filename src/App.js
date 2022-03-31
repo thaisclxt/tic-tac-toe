4f5404125a51bd3db4fc62e00367aaae360ebe02
thaisclxt <thais.calixto@mail.uft.edu.br>
2022-03-31 15:43:08 -0300

    create board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-	return <div className="App">{/* TO DO */}</div>;
+	return (
+		<div className="App">
+			<div className="board">{/* TO DO */}</div>
+		</div>
+	);
 }
 
 export default App;
