9c87d90a133b4b3aca49ad9934172714db15021b
thaisclxt <thais.calixto@mail.uft.edu.br>
2022-03-31 16:05:43 -0300

    create squares to first row

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+import { useState } from "react";
+import Square from "./components/Square";
 
 function App() {
+	const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+
 	return (
 		<div className="App">
 			<div className="board">
-				<div className="row"></div>
+				<div className="row">
+					<Square
+						val={board[0]}
+						chooseSquare={() => {
+							alert(0);
+						}}
+					/>
+					<Square
+						val={board[1]}
+						chooseSquare={() => {
+							alert(1);
+						}}
+					/>
+					<Square
+						val={board[2]}
+						chooseSquare={() => {
+							alert(2);
+						}}
+					/>
+				</div>
 				<div className="row"></div>
 				<div className="row"></div>
 			</div>
